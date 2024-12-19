eb753ceeaa9cf9a1af5e083962b41eeb7910217d
Natnael Yohannes <yonannesnatnael9@gmail.com>
2024-12-19 13:57:09 +0300

    main update

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ def main():
     parser.add_argument('domain', type=str, help='The target domain for enumeration and processing')
     args = parser.parse_args()
 
-
+    print(f"\nPerforming Subdomain Enumeration on {args.domain}")
     subdomains_file = run_sublister(args.domain)
-
+    print(f"\nSubdomains saved in {subdomains_file}")
     ## Process the subdomains
     print("\nProcessing the discovered subdomains...")
     resolve_domains_from_file(subdomains_file)
-    print(f"\nSubdomains saved in {subdomains_file}")
+    
     print("\nTrying subdomain takeover...")
     process_subdomains(subdomains_file)
 
